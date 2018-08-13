56f388ab8fde9909b67d972ab58c846cfb9f8d7b
far2na <enot.fx@gmail.com>
2018-08-13 20:45:33 +0300

    Added openning folders

--- app/src/main/java/com/google/enotfx/goodbookreader/MainActivity.java
+++ app/src/main/java/com/google/enotfx/goodbookreader/MainActivity.java
@@ -2,13 +2,20 @@ package com.google.enotfx.goodbookreader;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Environment;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,6 +24,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    private List<String> fileList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +37,37 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        List<String> fileList = new ArrayList<>();
-        File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
-        File[] files = root.listFiles();
-        fileList.clear();
-        for (File file : files){
-            fileList.add(file.getPath());
-        }
+        fileList = new ArrayList<>();
+        selectFolder(Environment.getExternalStorageDirectory().getAbsolutePath());
         ArrayAdapter<String> directoryList = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, fileList);
 
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(directoryList);
+        listView.setClickable(true);
+        OnItemClickListener mMessageClickedHandler = new OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id)
+            {
+                selectFolder(fileList.get(position));
+                ListView lv = findViewById(R.id.list_view);
+                lv.invalidateViews();
+            }
+        };
+        listView.setOnItemClickListener(mMessageClickedHandler);
+    }
+
+    public void selectFolder(String item) {
+        File root = new File(item);
+        File[] files = root.listFiles();
+        fileList.clear();
+        for (File file : files){
+            fileList.add(file.getPath());
+        }
+    }
+
+    public void addBook(View view) {
+        Button bt = findViewById(R.id.button2);
+        bt.setBackgroundColor(Color.RED);
     }
 }
 
